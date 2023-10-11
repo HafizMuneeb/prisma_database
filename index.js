4d269b366bd54bc940b8564554fde454573af70b
Muneeb <muneebafzal381a@gmail.com>
2023-10-11 15:07:18 +0500

    create cookie token for user

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+import cookieParser from 'cookie-parser';
 import express from 'express';
+import * as dotenv from 'dotenv';
+
+
+
+dotenv.config();
 const app = express();
+//regular middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+//cookie middleware
+
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.send('Hi there')
